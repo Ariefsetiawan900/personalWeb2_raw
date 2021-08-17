c86de250e87a9bb654450e7912574bd6b2fed315
Arief Setiawan <arief.setiawan8788@gmail.com>
2021-08-17 14:30:49 +0700

    setup responsive

--- Assets/js/script.js
+++ Assets/js/script.js
@@ -16,8 +16,8 @@ const toggleNavbar = () => {
 // =========== Active Section =============
 document.addEventListener("click", (e) => {
   if (e.target.classList.contains("link-item") && e.target.hash !== "") {
-    // active the overlay to prevent mutiple click 
-    document.querySelector(".overlay").classList.add("active")
+    // active the overlay to prevent mutiple click
+    document.querySelector(".overlay").classList.add("active");
     navToggler.classList.add("hide");
     if (e.target.classList.contains("nav-item")) {
       toggleNavbar();
@@ -33,7 +33,7 @@ document.addEventListener("click", (e) => {
       window.scrollTo(0, 0);
       document.body.classList.remove("hide-scrolling");
       navToggler.classList.remove("hide");
-      document.querySelector(".overlay").classList.remove("active")
+      document.querySelector(".overlay").classList.remove("active");
     }, 500);
   }
 });
